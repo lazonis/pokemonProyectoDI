762badf8a8152cb824c9413eafbb6f3c6d6fc760
zowi <xxx@example.com>
2026-01-05 14:27:27 +0100

    Limpiando rama master para añadir cambios estables

--- src/App.js
+++ src/App.js
@@ -6,25 +6,7 @@ function App() {
     <div className="App">
       <div className="App-header">
 
-        {
-          <PokemonCard
-            name={"Bulbasaur"}
-            image={"https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png"}
-            hp={100}
-            maxHp={100}
-            type={"verdura"}
-          />
-        }
-        {
-          <PokemonCard
-            name={"Pikachu" }
-            image={"https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Pok%C3%A9mon_Pikachu_art.png/250px-Pok%C3%A9mon_Pikachu_art.png"}
-            hp={100}
-            maxHp={100}
-            type={"tomas edison"}
-          />
-        }
-
+        
       </div>
     </div>
   );
