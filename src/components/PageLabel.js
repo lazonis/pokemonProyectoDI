2a9c70ad011a8393dad765c9bdd186f941ccb2fe
zowi <xxx@example.com>
2026-01-19 19:36:24 +0100

    Modified PageLabel frame

--- src/components/PageLabel.js
+++ src/components/PageLabel.js
@@ -3,9 +3,11 @@ import './PageLabel.css';
 
 function PageLabel({ title, subtitle }) {
     return (
-        <div className="page-label-container">
-            <h1 className="page-label-text">{title}</h1>
-            {subtitle && <p className="page-label-subtitle">{subtitle}</p>}
+        <div className='retro-image-label' style={{ borderImageSource: "url('label.png')" }}>
+            <div className="page-label-container" >
+                <h1 className="page-label-text">{title}</h1>
+                {subtitle && <p className="page-label-subtitle">{subtitle}</p>}
+            </div>
         </div>
     );
 }
