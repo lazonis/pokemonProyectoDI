4c0f0b0c9be544840326f3e02bd5a8510fe58937
zowi <xxx@example.com>
2026-01-20 15:26:31 +0100

    Fixed scroll from BattleLog div -> logContainerDef state -> hook useRef()

--- src/screens/battle/components/BattleLog.js
+++ src/screens/battle/components/BattleLog.js
@@ -2,22 +2,31 @@ import React, { useEffect, useRef } from 'react';
 import './BattleLog.css';
 
 const BattleLog = ({ logs }) => {
-    const logsEndRef = useRef(null);
+    const logContainerRef = useRef(null);
 
-    // Auto-scroll al fondo cada vez que llega un mensaje
+    // Auto-scroll al div log cada vez que llega un mensaje
     useEffect(() => {
-        logsEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, []);
+        if (logContainerRef.current) {
+            const { scrollHeight, clientHeight } = logContainerRef.current;
+            
+            // Hacemos que la posición del scroll sea igual a la altura total del contenido
+            // Esto lo fuerza siempre al final
+            logContainerRef.current.scrollTo({
+                top: scrollHeight - clientHeight,
+                behavior: 'smooth' 
+            });
+        }
+    }, [logs]);
 
     return (
-        <div className="log-container">
+        <div className="log-container" ref={logContainerRef}>
             <div className="log-list">
                 {logs.map((log, index) => (
                     <div key={index} className="log-entry">
                         {log}
                     </div>
                 ))}
-                <div ref={logsEndRef} />
+
             </div>
         </div>
     );
